f1ff8d2249619c04e35e103b4a014147a1926cf1
Gontran Lemaire <gontran.lemaire@gmail.com>
2019-08-12 13:00:06 +0200

    Add comments and update documentation

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const swaggerFile = require('./swagger.json');
 
 const app = express();
 
+// Expose OpenAPI service specification in Swagger UI
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile));
+
+// Expose the 'getTicket' method which generates a single ticket
 app.get('/getTicket', (req, resp) => {
     resp.status(200).send(getTicket());
 });
+
+// Expose the 'getTickets' method which generates as many tickets as it is requested by the 'count' query parameter 
 app.get('/getTickets', (req, resp) => {
     let tickets = [];
     for (let i = 0; i < req.query.count; i++) {
@@ -16,10 +21,12 @@ app.get('/getTickets', (req, resp) => {
     resp.status(200).send(tickets);
 });
 
+// Listen on port 3000
 app.listen(3000);
 
 console.log("Application started");
 
+// Function which generate a single ticket
 function getTicket() {
     const BASE34_ALPHABET = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ';
     var ticket = '';
