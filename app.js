f9357eefdf08ebf5f39fc484cd879e026682f7af
Gontran Lemaire <gontran.lemaire@gmail.com>
2019-08-12 10:58:26 +0200

    First functional version

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const swagger = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json');
 
 const app = express();
 
-app.get('/api/v1/ticket', (req, resp) => {
-    resp.status(200).send({
-        ticket: getTicket()
-    });
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile));
+app.get('/getTicket', (req, resp) => {
+    resp.status(200).send(getTicket());
+});
+app.get('/getTickets', (req, resp) => {
+    let tickets = [];
+    for (let i = 0; i < req.query.count; i++) {
+        tickets.push(getTicket());
+    }
+    resp.status(200).send(tickets);
 });
 
 app.listen(3000);
@@ -15,7 +23,7 @@ console.log("Application started");
 function getTicket() {
     const BASE34_ALPHABET = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ';
     var ticket = '';
-    for (let i = 0; i < 13; i++) {
+    while (ticket.length < 13) {
         ticket += BASE34_ALPHABET.charAt(Math.floor(Math.random() * BASE34_ALPHABET.length));
     }
     return ticket;
